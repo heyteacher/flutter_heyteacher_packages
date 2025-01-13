da314de0f4441c67d84f517e137ea0fc90d4aa9b
Hey, Teacher <heyteacher70@gmail.com>
2025-01-13 09:47:06 +0100

    calculate ad emit intensity

--- packages/flutter_heyteacher_utils/lib/ble/model/heart_rate_ble_model.dart
+++ packages/flutter_heyteacher_utils/lib/ble/model/heart_rate_ble_model.dart
@@ -1,13 +1,25 @@
+import 'package:flutter_heyteacher_utils/ble/data/ble_user_data.dart';
 import 'package:flutter_heyteacher_utils/ble/model/ble_model.dart';
 import 'package:flutter_heyteacher_utils/ble/data/enums.dart';
 import 'package:flutter_heyteacher_utils/ble/model/ble_model_factory.dart';
+import 'package:flutter_heyteacher_utils/ble/store/ble_user_store.dart';
+import 'package:flutter_heyteacher_utils/firebase/auth.dart';
 //import 'package:logging/logging.dart';
 
 class HeartRateBleModel extends BleModel {
   //Logger _log = Logger("HeartRateBleModel");
-  
+  BleUserData? _bleUserData;
+
   @override
-  void onInit() {}
+  void onInit() async {
+    // initialize _bleUserData
+    _bleUserData = await BleUserStore.instance().get(Auth.instance().uid!);
+    // listen login and initialize _bleUserData 
+    Auth.instance()
+        .stateChangesStream
+        .where((user) => user != null)
+        .listen((user) async => _bleUserData = await BleUserStore.instance().get(user!.uid));
+  }
 
   static HeartRateBleModel? _instance;
   static HeartRateBleModel get instance => _instance ??= HeartRateBleModel._();
@@ -17,7 +29,12 @@ class HeartRateBleModel extends BleModel {
   void onData(List<int> event) {
     if (event.length >= 2) {
       if (event[2] > 0) {
-        streamController.sink.add({BleModelFactory.streamKey: event[1].toString()});
+        int bpm = event[1];
+        int? intensity = _bleUserData?.intensity(bpm);
+        streamController.sink.add({
+          BleModelFactory.streamKey:
+              "$bpm${intensity != null ? "\n$intensity%" : ""}"
+        });
       }
       // _log.fine("bpm ${event[1]}");
     }
