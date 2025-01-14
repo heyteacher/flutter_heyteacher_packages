7eb229eac876407e90774982d26330d3bddae133
Hey, Teacher <heyteacher70@gmail.com>
2025-01-14 12:26:40 +0100

    use record in streams

--- packages/flutter_heyteacher_utils/lib/ble/model/heart_rate_ble_model.dart
+++ packages/flutter_heyteacher_utils/lib/ble/model/heart_rate_ble_model.dart
@@ -1,25 +1,13 @@
 import 'package:flutter_heyteacher_utils/ble/data/ble_user_data.dart';
 import 'package:flutter_heyteacher_utils/ble/model/ble_model.dart';
 import 'package:flutter_heyteacher_utils/ble/data/enums.dart';
-import 'package:flutter_heyteacher_utils/ble/model/ble_model_factory.dart';
-import 'package:flutter_heyteacher_utils/ble/store/ble_user_store.dart';
-import 'package:flutter_heyteacher_utils/firebase/auth.dart';
 //import 'package:logging/logging.dart';
 
 class HeartRateBleModel extends BleModel {
   //Logger _log = Logger("HeartRateBleModel");
-  BleUserData? _bleUserData;
 
   @override
-  void onInit() async {
-    // initialize _bleUserData
-    _bleUserData = await BleUserStore.instance().get(Auth.instance().uid!);
-    // listen login and initialize _bleUserData 
-    Auth.instance()
-        .stateChangesStream
-        .where((user) => user != null)
-        .listen((user) async => _bleUserData = await BleUserStore.instance().get(user!.uid));
-  }
+  void onInit() async {}
 
   static HeartRateBleModel? _instance;
   static HeartRateBleModel get instance => _instance ??= HeartRateBleModel._();
@@ -28,13 +16,16 @@ class HeartRateBleModel extends BleModel {
   @override
   void onData(List<int> event) {
     if (event.length >= 2) {
-      if (event[2] > 0) {
+      if (event[1] > 0) {
         int bpm = event[1];
-        int? intensity = _bleUserData?.intensity(bpm);
-        streamController.sink.add({
-          BleModelFactory.streamKey:
-              "$bpm${intensity != null ? "\n$intensity%" : ""}"
-        });
+        int? intensityValue = intensity(bpm);
+        streamController.sink.add((
+          value: bpm,
+          formatted: bpm.toString(),
+          subValue: intensityValue,
+          subFormatted: intensityValue != null ? "$intensity%" : "",
+          color: HeartRateTrainingZone.intensityColor(intensityValue)
+        ));
       }
       // _log.fine("bpm ${event[1]}");
     }
