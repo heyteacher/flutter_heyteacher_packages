f7fe529d6c482cc171ca62fe2105407b558b5668
Hey, Teacher <heyteacher70@gmail.com>
2025-01-16 14:12:20 +0100

    add z0 and z6 training zones

--- packages/flutter_heyteacher_utils/lib/ble/data/ble_user_data.dart
+++ packages/flutter_heyteacher_utils/lib/ble/data/ble_user_data.dart
@@ -5,100 +5,6 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:flutter_heyteacher_utils/firebase/firestore/user_store.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-enum BleType {
-  heartRate(
-      icon: FontAwesomeIcons.heartPulse,
-      firestoreFieldId: "heartRateDeviceId",
-      firestoreFieldName: "heartRateDeviceName",
-      uuidService: "180d",
-      uuidCharacteristic:"2a37"),
-  cadence(
-      icon: Icons.change_circle,
-      firestoreFieldId: "cadenceDeviceId",
-      firestoreFieldName: "cadenceDeviceName",
-            uuidService: "1816",
-      uuidCharacteristic:"2a5b");
-
-  const BleType(
-      {required this.icon,
-      required this.firestoreFieldId,
-      required this.firestoreFieldName,
-      required this.uuidService,
-      required this.uuidCharacteristic});
-  final IconData icon;
-  final String firestoreFieldId;
-  final String firestoreFieldName;
-  final String uuidService;
-  final String uuidCharacteristic;
-}
-
-enum Gender {
-  male(heartRateCoeff: 220),
-  female(heartRateCoeff: 226),
-  other(heartRateCoeff: 223);
-
-  final int heartRateCoeff;
-  const Gender({required this.heartRateCoeff});
-
-  @override
-  String toString() {
-    return name;
-  }
-}
-
-enum HeartRateTrainingZone {
-  z1(minIntensity: 50, maxIntensity: 60, color: Colors.cyanAccent),
-  z2(minIntensity: 60, maxIntensity: 70, color: Colors.greenAccent),
-  z3(minIntensity: 70, maxIntensity: 80, color: Colors.yellowAccent),
-  z4(minIntensity: 80, maxIntensity: 90, color: Colors.orangeAccent),
-  z5(minIntensity: 90, maxIntensity: 100, color: Colors.redAccent);
-
-  final int minIntensity;
-  final int maxIntensity;
-  final Color color;
-  const HeartRateTrainingZone(
-      {required this.minIntensity,
-      required this.maxIntensity,
-      required this.color});
-
-  static HeartRateTrainingZone? fromIntensity(int? intensity) =>
-      HeartRateTrainingZone.values
-          .where((zone) =>
-              (intensity ?? 0) >= zone.minIntensity &&
-              (intensity ?? 0) < zone.maxIntensity)
-          .firstOrNull;
-
-  ({HeartRateTrainingZone heartRateTrainingZone, int? min, int? max}) targetBpm(
-          {({Gender? gender, int? age, int? restBpm})? biometrics}) =>
-      (
-        heartRateTrainingZone: this,
-        min: _targetBpm(biometrics: biometrics, intensity: minIntensity),
-        max: _targetBpm(biometrics: biometrics, intensity: maxIntensity)
-      );
-
-  // targetBpm = [((female: 226| male: 200) - age - restBpm) x intensity% \ 100] + restBpm
-  int? _targetBpm(
-          {required ({Gender? gender, int? age, int? restBpm})? biometrics,
-          required int? intensity}) =>
-      biometrics?.gender != null &&
-              biometrics?.age != null &&
-              biometrics?.restBpm != null &&
-              intensity != null
-          ? (((biometrics!.gender!.heartRateCoeff -
-                          biometrics.age! -
-                          biometrics.restBpm!) *
-                      intensity /
-                      100) +
-                  biometrics.restBpm!)
-              .round()
-          : null;
-}
-
-class CrankRevolutionRecordData {
-  DateTime timestamp;
-  int counter;
-  CrankRevolutionRecordData({required this.timestamp, required this.counter});
-}
 
 class BleUserData extends UserData {
   Map<BleType, ({String? id, String? name})>? devices;
@@ -187,4 +93,101 @@ class BleUserData extends UserData {
   String toString() => "${super.toString()}, "
       "devices: ${devices?.map((key, value) => MapEntry(key.name, "${value.name} (${value.id})"))}, "
       "biometrics: $biometrics";
+}
+
+enum BleType {
+  heartRate(
+      icon: FontAwesomeIcons.heartPulse,
+      firestoreFieldId: "heartRateDeviceId",
+      firestoreFieldName: "heartRateDeviceName",
+      uuidService: "180d",
+      uuidCharacteristic:"2a37"),
+  cadence(
+      icon: Icons.change_circle,
+      firestoreFieldId: "cadenceDeviceId",
+      firestoreFieldName: "cadenceDeviceName",
+            uuidService: "1816",
+      uuidCharacteristic:"2a5b");
+
+  const BleType(
+      {required this.icon,
+      required this.firestoreFieldId,
+      required this.firestoreFieldName,
+      required this.uuidService,
+      required this.uuidCharacteristic});
+  final IconData icon;
+  final String firestoreFieldId;
+  final String firestoreFieldName;
+  final String uuidService;
+  final String uuidCharacteristic;
+}
+
+enum Gender {
+  male(heartRateCoeff: 220),
+  female(heartRateCoeff: 226),
+  other(heartRateCoeff: 223);
+
+  final int heartRateCoeff;
+  const Gender({required this.heartRateCoeff});
+
+  @override
+  String toString() {
+    return name;
+  }
+}
+
+enum HeartRateTrainingZone {
+  z0(minIntensity: 0, maxIntensity: 50, color: Colors.white70),
+  z1(minIntensity: 50, maxIntensity: 60, color: Colors.cyanAccent),
+  z2(minIntensity: 60, maxIntensity: 70, color: Colors.greenAccent),
+  z3(minIntensity: 70, maxIntensity: 80, color: Colors.yellowAccent),
+  z4(minIntensity: 80, maxIntensity: 90, color: Colors.orangeAccent),
+  z5(minIntensity: 90, maxIntensity: 100, color: Colors.redAccent),
+  z6(minIntensity: 100, maxIntensity: 1000, color: Colors.purpleAccent);
+
+  final int minIntensity;
+  final int maxIntensity;
+  final Color color;
+  const HeartRateTrainingZone(
+      {required this.minIntensity,
+      required this.maxIntensity,
+      required this.color});
+
+  static HeartRateTrainingZone? fromIntensity(int? intensity) =>
+      HeartRateTrainingZone.values
+          .where((zone) =>
+              (intensity ?? 0) >= zone.minIntensity &&
+              (intensity ?? 0) < zone.maxIntensity)
+          .firstOrNull;
+
+  ({HeartRateTrainingZone heartRateTrainingZone, int? min, int? max}) targetBpm(
+          {({Gender? gender, int? age, int? restBpm})? biometrics}) =>
+      (
+        heartRateTrainingZone: this,
+        min: _targetBpm(biometrics: biometrics, intensity: minIntensity),
+        max: _targetBpm(biometrics: biometrics, intensity: maxIntensity)
+      );
+
+  // targetBpm = [((female: 226| male: 200) - age - restBpm) x intensity% \ 100] + restBpm
+  int? _targetBpm(
+          {required ({Gender? gender, int? age, int? restBpm})? biometrics,
+          required int? intensity}) =>
+      biometrics?.gender != null &&
+              biometrics?.age != null &&
+              biometrics?.restBpm != null &&
+              intensity != null
+          ? (((biometrics!.gender!.heartRateCoeff -
+                          biometrics.age! -
+                          biometrics.restBpm!) *
+                      intensity /
+                      100) +
+                  biometrics.restBpm!)
+              .round()
+          : null;
+}
+
+class CrankRevolutionRecordData {
+  DateTime timestamp;
+  int counter;
+  CrankRevolutionRecordData({required this.timestamp, required this.counter});
 }
