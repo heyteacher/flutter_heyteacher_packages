7f2212f92b0e8d851f57fc8ccb0f2e1c6113cdae
Hey, Teacher <heyteacher70@gmail.com>
2025-01-31 08:15:11 +0100

    review tts model code

--- packages/flutter_heyteacher_utils/lib/tts/model/tts_model.dart
+++ packages/flutter_heyteacher_utils/lib/tts/model/tts_model.dart
@@ -8,45 +8,45 @@ import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
 
 class TtsModel {
-  static final _log = Logger("TTSModel");
+  static final _log = Logger("TtsModel");
 
   late FlutterTts _textToSpeech;
 
-  StreamSubscription? _streamSubscription;
+  StreamSubscription? _onUserUpdateStreamSubscription,
+      _stateChangesStreamSubscription;
 
   static TtsModel? _instance;
   static TtsModel get instance => _instance ??= TtsModel._();
   TtsModel._() {
     _log.fine("costructor");
     _textToSpeech = FlutterTts();
-    _setAwaitOptions();
-    _streamSubscription?.cancel();
-    Auth.instance().stateChangesStream.listen((_) async => _changeLanguage(
-        Auth.instance().autenticated
+    _textToSpeech.awaitSpeakCompletion(true);
+    // on authentication user
+    _stateChangesStreamSubscription?.cancel();
+    _stateChangesStreamSubscription = Auth.instance().stateChangesStream.listen(
+        (_) async => _changeLanguage(Auth.instance().autenticated
             ? (await UserStore.instance().getOrNull(Auth.instance().uid))
                 ?.locale
                 ?.languageCode
             : null));
-    _streamSubscription = UserStore.instance()
+    // on change unpdate user profile
+    _onUserUpdateStreamSubscription?.cancel();
+    _onUserUpdateStreamSubscription = UserStore.instance()
         .onUserUpdated
         .listen((user) => _changeLanguage(user.locale?.languageCode));
   }
 
   dispose() {
-    _streamSubscription?.cancel();
+    _stateChangesStreamSubscription?.cancel();
+    _onUserUpdateStreamSubscription?.cancel();
   }
 
-  Future<void> speak(String text, {double speechRate = 0}) async {
+  Future<void> speak(String text, {double speechRate = 1}) async {
     _log.fine("speak({speechRate:$speechRate}): $text");
     await _textToSpeech.setSpeechRate(speechRate);
     _textToSpeech.speak(text);
   }
 
-  Future<void> _setAwaitOptions() async {
-    _textToSpeech.awaitSpeakCompletion(true);
-  }
-
-  void _changeLanguage(String? languageCode) {
-    _textToSpeech.setLanguage(languageCode ?? Intl.getCurrentLocale());
-  }
+  void _changeLanguage(String? languageCode) async =>
+      await _textToSpeech.setLanguage(languageCode ?? Intl.getCurrentLocale());
 }
