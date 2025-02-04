8bf7ec678956ee0a81dc27cd3316b0705078328b
Hey, Teacher <heyteacher70@gmail.com>
2025-02-04 17:25:31 +0100

    remove unnecessary this.

--- packages/flutter_heyteacher_utils/lib/firebase/firestore/store.dart
+++ packages/flutter_heyteacher_utils/lib/firebase/firestore/store.dart
@@ -895,6 +895,6 @@ class ParentDataNullException {
 }
 extension StringExtension on String {
     String capitalize() {
-      return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+      return "${this[0].toUpperCase()}${substring(1).toLowerCase()}";
     }
 }
