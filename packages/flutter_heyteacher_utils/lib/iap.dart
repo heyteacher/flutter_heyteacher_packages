889cdfe0b8a8a36967bb84bbd9c9c5255bbd9f3c
Hey, Teacher <heyteacher70@gmail.com>
2025-04-17 13:23:17 +0200

    export subscription_purchase_store used in in integration test

--- packages/flutter_heyteacher_utils/lib/iap.dart
+++ packages/flutter_heyteacher_utils/lib/iap.dart
@@ -4,3 +4,4 @@ export "src/iap/iap_plan.dart" show IAPPlan;
 export 'src/iap/iap_model.dart' show IapModel;
 export 'src/iap/subscription_purchase_store.dart' show SubscriptionPurchaseData;
 export 'src/iap/iap_screen.dart' show IapScreen, SubscriptionPurchaseWidget;
+export 'src/iap/subscription_purchase_store.dart' show SubscriptionPurchaseStore;
