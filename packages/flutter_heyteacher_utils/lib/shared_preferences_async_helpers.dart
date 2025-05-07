94016ed21ec4b5ab49d57c359ab98cb02f1de0a7
Hey, Teacher <heyteacher70@gmail.com>
2025-05-07 16:46:12 +0200

    remove: delete SharedPreferencesAsyncHelpers extension

--- packages/flutter_heyteacher_utils/lib/shared_preferences_async_helpers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-extension SharedPreferencesAsyncHelpers on SharedPreferencesAsync {
-  Future<String> getStringWithDefault(String key, String defaultValue) async =>
-      await getString(key) ?? defaultValue;
-}
