a7ae856260cf16d2ef1fd99991448e38ddcb0deb
Hey, Teacher <heyteacher70@gmail.com>
2025-05-10 14:58:36 +0200

    feat: add ContextHelper for accessing BuildContext outside widgets

--- packages/flutter_heyteacher_utils/lib/context_helper.dart
+++ packages/flutter_heyteacher_utils/lib/context_helper.dart
@@ -1,10 +1,26 @@
 // somewhere else in your project
 import 'package:flutter/material.dart';
 
+/// Context helper for get [BuildContext] outside widget, for example getting
+/// localization in business logic.
+/// 
+/// To use it, need to be set [scaffoldMessengerKey] in your app 
+/// ```
+/// class MyApp extends StatelessWidget {
+///   
+///   @override
+///   Widget build(BuildContext context) {
+///     return MaterialApp.router(
+///       scaffoldMessengerKey: ContextHelper.scaffoldMessengerKey,
+///       ...
+///       ...
+///       ...
+/// ```
 class ContextHelper {
   static final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
       GlobalKey<ScaffoldMessengerState>();
 
+  /// get the context
   static BuildContext? get context =>
       scaffoldMessengerKey.currentContext;
 
