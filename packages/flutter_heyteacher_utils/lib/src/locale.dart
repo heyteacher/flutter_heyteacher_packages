3af212d035b301898832d617d25b01c3882b8c15
Hey, Teacher <heyteacher70@gmail.com>
2025-05-12 08:02:40 +0200

    refactor: enhance LocaleModel for better locale management and update LocaleListTile to use stateful widget

--- packages/flutter_heyteacher_utils/lib/src/locale.dart
+++ packages/flutter_heyteacher_utils/lib/src/locale.dart
@@ -5,9 +5,20 @@ import 'package:flutter_heyteacher_utils/src/l10n/flutter_heyteacher_utils.dart'
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleModel {
+  Locale? _locale;
+
   static LocaleModel? _instance;
   static LocaleModel get instance => _instance ??= LocaleModel._();
-  LocaleModel._();
+
+  LocaleModel._() {
+    SharedPreferencesAsync().getString('locale').then((localeName) {
+      _locale = FlutterHeyteacherUtilsLocalizations.supportedLocales
+          .firstWhere((locale) => locale.languageCode == localeName);
+      if (_locale == null) {
+        _localeStreamController.sink.add(_locale!);
+      }
+    });
+  }
 
   final StreamController<Locale> _localeStreamController =
       StreamController<Locale>.broadcast();
@@ -15,36 +26,47 @@ class LocaleModel {
   /// stream yield [Locale] changes
   Stream<Locale> get localeStream => _localeStreamController.stream;
 
-  void setLocale(Locale locale) async {
-    SharedPreferencesAsync().setString('locale', locale.languageCode);
-    _localeStreamController.sink.add(locale);
+  set locale(Locale? newLocale) {
+    _locale = newLocale;
+    if (newLocale != null) {
+      SharedPreferencesAsync().setString('locale', newLocale.languageCode);
+      _localeStreamController.sink.add(newLocale);
+    } else {
+      SharedPreferencesAsync().remove('locale');
+    }
   }
+
+  Locale? get locale => _locale;
 }
 
-class LocaleListTile extends StatelessWidget {
+class LocaleListTile extends StatefulWidget {
   const LocaleListTile({super.key});
 
+  @override
+  State<LocaleListTile> createState() => _LocaleListTileState();
+}
+
+class _LocaleListTileState extends State<LocaleListTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
         key: ValueKey("lt_locale"),
-        leading: Icon(
-          Icons.language,
-          size: Theme.of(context).textTheme.displaySmall!.fontSize,
-        ),
         title: SegmentedButton<Locale>(
             segments: <ButtonSegment<Locale>>[
-              for (Locale locale in FlutterHeyteacherUtilsLocalizations.supportedLocales)
+              for (Locale locale
+                  in FlutterHeyteacherUtilsLocalizations.supportedLocales)
                 ButtonSegment<Locale>(
                   value: locale,
-                  label: Text(locale.languageCode.toUpperCase()),
+                  label: Text(locale.languageCode),
                 )
             ],
             selected: <Locale>{
-              Localizations.localeOf(context)
+              LocaleModel.instance.locale ?? Localizations.localeOf(context)
             },
             onSelectionChanged: (Set<Locale> newSelection) async {
-              LocaleModel.instance.setLocale(newSelection.first);
+              setState(() {
+              LocaleModel.instance.locale = newSelection.first;
+              });
             }));
   }
 }
