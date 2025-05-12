862f3a0f15d377c3c9ce5b2b2d90597b66354440
Hey, Teacher <heyteacher70@gmail.com>
2025-05-12 12:25:40 +0200

    refactor: consolidate device and package info retrieval into InfoDevicePackageModel

--- packages/flutter_heyteacher_utils/lib/info_device_package.dart
+++ packages/flutter_heyteacher_utils/lib/info_device_package.dart
@@ -1,3 +1,5 @@
+library;
+
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -8,29 +10,30 @@ import 'package:flutter_heyteacher_utils/theme.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+/// A widget that displays device and package information in a list tile format.
+///
+/// It shows the device type, version, and a button to ask for support.
 class DevicePackageInfoListTile extends StatelessWidget {
   const DevicePackageInfoListTile({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      key: ValueKey("lt_version"),
+      key: ValueKey("lt_fhu_version"),
       leading: Icon(
         Icons.smartphone,
-        size: Theme.of(context).textTheme.displayMedium!.fontSize,
       ),
       title: FutureBuilder(
-        future: deviceInfo,
+        future: InfoDevicePackageModel.instance.deviceInfo,
         builder: (_, deviceSnapshot) =>
             Text("${FlutterHeyteacherUtilsLocalizations.of(context)!.id}"
-                "$identifierInfo-${deviceSnapshot.data}"),
+                "${deviceSnapshot.data}-${deviceSnapshot.data}"),
       ),
       subtitle: FutureBuilder<String>(
-        future: packageVersion,
-        builder: (_, snapshot) => Text(snapshot.data != null
-            ? "${FlutterHeyteacherUtilsLocalizations.of(context)!.version}"
-                "${snapshot.data}"
-            : ""),
+        future: InfoDevicePackageModel.instance.packageVersion,
+        builder: (_, devicePackageSnapshot) =>
+            Text("${FlutterHeyteacherUtilsLocalizations.of(context)!.version}"
+                "${devicePackageSnapshot.data}"),
       ),
       trailing: TextButton(
           style: TextButton.styleFrom(
@@ -44,49 +47,15 @@ class DevicePackageInfoListTile extends StatelessWidget {
   }
 }
 
-Future<String> get deviceInfo  async {
-  final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-  if (kIsWeb) {
-    final webDeviceInfo = await deviceInfoPlugin.webBrowserInfo;
-    return "w-${webDeviceInfo.browserName}-ua-${webDeviceInfo.userAgent}";
-  }
-  switch (defaultTargetPlatform) {
-    case TargetPlatform.android:
-      {
-        final androidDeviceInfo = await deviceInfoPlugin.androidInfo;
-        return "a-${androidDeviceInfo.model}-"
-            "sdk-${androidDeviceInfo.version.sdkInt}-"
-            "${androidDeviceInfo.version.release}";
-      }
-    case TargetPlatform.iOS:
-      {
-        final iosDeviceInfo = await deviceInfoPlugin.iosInfo;
-        return "i-${iosDeviceInfo.model}-"
-            "sysver-${iosDeviceInfo.systemVersion}";
-      }
-    default:
-      {
-        return defaultTargetPlatform.name;
-      }
-  }
-}
-
-Future<String> get packageVersion async {
-  final packageInfoPlatform = await PackageInfo.fromPlatform();
-  return "${packageInfoPlatform.version}+${packageInfoPlatform.buildNumber}";
-}
-
-String get identifierInfo => (Auth.instance().uid?.substring(0, 5)) ?? "guest";
-
 void _askSupport() async {
   final packageInfoPlatform = await PackageInfo.fromPlatform();
-  final version = await packageVersion;
-  final device = await deviceInfo;
+  final version = await InfoDevicePackageModel.instance.packageVersion;
+  final device = await InfoDevicePackageModel.instance.deviceInfo;
   final subject =
       "${FlutterHeyteacherUtilsLocalizations.of(ContextHelper.context!)!.askSupportFor}"
       "${packageInfoPlatform.appName}";
   final body = "------------------------------------\n"
-      "Identifier:\t$identifierInfo-$device\n"
+      "Identifier:\t${InfoDevicePackageModel.instance.identifierInfo}-$device\n"
       "Version:\t$version\n"
       "------------------------------------\n"
       "\n";
@@ -96,4 +65,46 @@ void _askSupport() async {
     query: "subject=$subject&body=${Uri.encodeFull(body)}",
   );
   launchUrl(uri);
+}
+
+class InfoDevicePackageModel {
+  static InfoDevicePackageModel? _instance;
+  static InfoDevicePackageModel get instance =>
+      _instance ??= InfoDevicePackageModel._();
+  InfoDevicePackageModel._();
+
+  Future<String> get deviceInfo async {
+    final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+    if (kIsWeb) {
+      final webDeviceInfo = await deviceInfoPlugin.webBrowserInfo;
+      return "w-${webDeviceInfo.browserName}-ua-${webDeviceInfo.userAgent}";
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        {
+          final androidDeviceInfo = await deviceInfoPlugin.androidInfo;
+          return "a-${androidDeviceInfo.model}-"
+              "sdk-${androidDeviceInfo.version.sdkInt}-"
+              "${androidDeviceInfo.version.release}";
+        }
+      case TargetPlatform.iOS:
+        {
+          final iosDeviceInfo = await deviceInfoPlugin.iosInfo;
+          return "i-${iosDeviceInfo.model}-"
+              "sysver-${iosDeviceInfo.systemVersion}";
+        }
+      default:
+        {
+          return defaultTargetPlatform.name;
+        }
+    }
+  }
+
+  Future<String> get packageVersion async {
+    final packageInfoPlatform = await PackageInfo.fromPlatform();
+    return "${packageInfoPlatform.version}+${packageInfoPlatform.buildNumber}";
+  }
+
+  String get identifierInfo =>
+      (Auth.instance().uid?.substring(0, 5)) ?? "guest";
 }
