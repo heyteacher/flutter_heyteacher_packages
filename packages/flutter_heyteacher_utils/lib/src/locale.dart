24a8cd33e2285c7b59ee481904167852ebc004b6
Hey, Teacher <heyteacher70@gmail.com>
2025-05-12 12:14:03 +0200

    refactor: enhance LocaleListTile with ChoiceChip for locale selection

--- packages/flutter_heyteacher_utils/lib/src/locale.dart
+++ packages/flutter_heyteacher_utils/lib/src/locale.dart
@@ -5,7 +5,7 @@ import 'package:flutter_heyteacher_utils/src/l10n/flutter_heyteacher_utils.dart'
 import 'package:shared_preferences/shared_preferences.dart';
 
 /// The [Locale] list tile widget.
-/// 
+///
 /// This widget is used to select the locale supported by the app.
 class LocaleListTile extends StatefulWidget {
   const LocaleListTile({super.key});
@@ -18,34 +18,38 @@ class _LocaleListTileState extends State<LocaleListTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+        leading: const Icon(Icons.language),
         key: ValueKey("lt_fhu_locale"),
-        title: SegmentedButton<Locale>(
-            segments: <ButtonSegment<Locale>>[
-              for (Locale locale
-                  in FlutterHeyteacherUtilsLocalizations.supportedLocales)
-                ButtonSegment<Locale>(
-                  value: locale,
-                  label: Text(locale.languageCode),
-                )
-            ],
-            selected: <Locale>{
-              LocaleModel.instance.locale ?? Localizations.localeOf(context)
-            },
-            onSelectionChanged: (Set<Locale> newSelection) async {
-              LocaleModel.instance.locale = newSelection.first;
-              setState(() {});
-            }));
+        title: Wrap(
+          alignment: WrapAlignment.spaceEvenly,
+          spacing: 2,
+          children: [
+            ...FlutterHeyteacherUtilsLocalizations.supportedLocales
+                .map<Widget>((locale) => ChoiceChip(
+                      label: Text(locale.languageCode),
+                      selected: locale ==
+                          (LocaleModel.instance.locale ??
+                              Localizations.localeOf(context)),
+                      onSelected: (bool selected) {
+                        setState(() {
+                          LocaleModel.instance.locale =
+                              selected ? locale : null;
+                        });
+                      },
+                    )),
+          ],
+        ));
   }
 }
 
 /// The [Locale] model class is used to manage the app's supporded locale.
-/// 
+///
 /// The locale is saved in the [SharedPreferencesAsync] on key `fhu_locale`.
 /// Locale changes are yield on [localeStream].
 class LocaleModel {
   Locale? _locale;
 
-  static const  _sharedPreferencesLocaleKey = 'fhuLocale';
+  static const _sharedPreferencesLocaleKey = 'fhuLocale';
 
   static LocaleModel? _instance;
   static LocaleModel get instance => _instance ??= LocaleModel._();
@@ -55,9 +59,12 @@ class LocaleModel {
   /// This constructor loads the saved locale from [SharedPreferencesAsync]
   LocaleModel._() {
     // Load the saved locale from SharedPreferences
-    SharedPreferencesAsync().getString(_sharedPreferencesLocaleKey).then((localeName) {
+    SharedPreferencesAsync()
+        .getString(_sharedPreferencesLocaleKey)
+        .then((localeName) {
       _locale = FlutterHeyteacherUtilsLocalizations.supportedLocales
-          .where((locale) => locale.languageCode == localeName).firstOrNull;
+          .where((locale) => locale.languageCode == localeName)
+          .firstOrNull;
       if (_locale != null) {
         _localeStreamController.sink.add(_locale!);
       }
@@ -74,14 +81,15 @@ class LocaleModel {
   Locale? get locale => _locale;
 
   /// set the [Locale] and save it to SharedPreferences.
-  /// 
+  ///
   /// If [newLocale] is null, remove the saved locale from SharedPreferences
-  /// and set the locale to null. If [newLocale] is not null, yield the new 
+  /// and set the locale to null. If [newLocale] is not null, yield the new
   /// locale to the stream
   set locale(Locale? newLocale) {
     _locale = newLocale;
     if (newLocale != null) {
-      SharedPreferencesAsync().setString(_sharedPreferencesLocaleKey, newLocale.languageCode);
+      SharedPreferencesAsync()
+          .setString(_sharedPreferencesLocaleKey, newLocale.languageCode);
       _localeStreamController.sink.add(newLocale);
     } else {
       SharedPreferencesAsync().remove(_sharedPreferencesLocaleKey);
