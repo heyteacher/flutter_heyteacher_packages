87cd23fbb1d345ce2cc3e47c0036d59893edb77e
Hey, Teacher <heyteacher70@gmail.com>
2025-07-03 12:41:44 +0200

    rename CrashlyticsModel in CrashlyticsModelView

--- packages/flutter_heyteacher_utils/lib/firebase.dart
+++ packages/flutter_heyteacher_utils/lib/firebase.dart
@@ -12,5 +12,5 @@ library;
 export 'src/firebase/app_check.dart' show AppCheckModelView;
 export 'src/firebase/auth.dart'
     show AuthModelView, UserNotAuthenticatedException, AccountCard;
-export 'src/firebase/crashlytics.dart' show CrashlyticsModel;
+export 'src/firebase/crashlytics.dart' show CrashlyticsModelView;
 export 'src/firebase/remote_config.dart' show RemoteConfigModel;
