e12e462be4c062c063067f96c2928e3adba0baf2
Hey, Teacher <heyteacher70@gmail.com>
2025-07-21 22:04:24 +0200

    long running isolate implementation

---
+++ packages/flutter_heyteacher_utils/lib/worker.dart
@@ -0,0 +1,8 @@
+library;
+/// A library for creating and managing long-running isolates.
+///
+/// This library provides the [Worker] class, an abstraction for offloading
+/// computationally intensive tasks to a background isolate, thus keeping the
+/// main UI thread responsive.
+
+export 'src/worker.dart' show Worker;
