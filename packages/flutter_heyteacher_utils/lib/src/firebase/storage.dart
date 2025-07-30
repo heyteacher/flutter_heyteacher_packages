a67dcc0eda0b2a5a72f5e4b4574b96ce85b20d52
Hey, Teacher <heyteacher70@gmail.com>
2025-07-30 09:53:29 +0200

    manage use rnon autenticated during store a file into Storage +  fix logging format

--- packages/flutter_heyteacher_utils/lib/src/firebase/storage.dart
+++ packages/flutter_heyteacher_utils/lib/src/firebase/storage.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_heyteacher_utils/firebase.dart';
 import 'package:logging/logging.dart';
 
 class StorageViewModel {
@@ -12,22 +13,30 @@ class StorageViewModel {
 
   final _storage = FirebaseStorage.instance;
 
-  Future<String> uploadString(String filePath, String fileContent) async {
+  Future<String?> uploadString(String filePath, String fileContent) async {
+    log.finest('<uploadFile>: filePath $filePath');
     try {
-      log.info('uploadFile: upload file $filePath');
+      if (AuthViewModel.instance().notAutenticated) {
+        log.warning('(uploadFile): filePath $filePath. User not authenticated, '
+            'cannot upload file in Storage');
+        return null;
+      }
       final storageRef = _storage.ref();
       final fileRef = storageRef.child(filePath);
       await fileRef.putString(fileContent);
-      return fileRef.getDownloadURL();
-    } on FirebaseException catch (e, s) {
-      log.severe('uploadFile: failed to upload file $filePath', e, s);
+      final downloadURL = await fileRef.getDownloadURL();
+      log.info('(uploadFile): filePath $filePath. File uploaded successfully '
+          '$downloadURL');
+      return downloadURL;
+    } on FirebaseException catch (error, stackTrace) {
+      log.severe(
+          '(uploadFile): filePath $filePath. Failed tu upload file in Storage', error, stackTrace);
       throw UploadStorageException(filePath);
     }
   }
 }
 
 class UploadStorageException implements Exception {
-
   String filePath;
 
   UploadStorageException(this.filePath);
