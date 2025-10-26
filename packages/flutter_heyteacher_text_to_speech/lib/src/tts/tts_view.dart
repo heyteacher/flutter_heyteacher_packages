ab1c069c8dfe55a2822e9c70efef0d1d55f723fb
Hey, Teacher <heyteacher70@gmail.com>
2025-10-26 16:18:27 +0100

    apply very good analysis

--- packages/flutter_heyteacher_text_to_speech/lib/src/tts/tts_view.dart
+++ packages/flutter_heyteacher_text_to_speech/lib/src/tts/tts_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_heyteacher_text_to_speech/src/l10n/flutter_heyteacher_text_to_speech.dart';
 import 'package:flutter_heyteacher_text_to_speech/src/tts/tts_data.dart';
@@ -5,7 +7,14 @@ import 'package:flutter_heyteacher_utils/firebase.dart';
 import 'package:flutter_heyteacher_utils/locale.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// A card widget with a switch to enable or disable Text-To-Speech (TTS).
+///
+/// This widget displays a `ListTile` with a title, a subtitle showing the
+/// default TTS setting from remote config, and a trailing `Switch` to let the
+/// user override the setting. The user's preference is persisted in
+/// [SharedPreferences].
 class EnableTTSDropDownMenuCard extends StatefulWidget {
+  /// Creates an [EnableTTSDropDownMenuCard] widget.
   const EnableTTSDropDownMenuCard({super.key});
 
   @override
@@ -23,7 +32,7 @@ class _EnableTTSDropDownMenuCardState extends State<EnableTTSDropDownMenuCard> {
         child: ListTile(
           leading: const Icon(Icons.speaker_phone),
           title: Text(FlutterHeyteacherTextToSpeechLocalizations.of(context)!
-              .enableTextToSpeech),
+              .enableTextToSpeech,),
           subtitle: Text(
             FlutterHeyteacherUtilsLocalizations.of(context)!.defaultValue(
               RemoteConfigViewModel.instance.getBool(
@@ -40,10 +49,10 @@ class _EnableTTSDropDownMenuCardState extends State<EnableTTSDropDownMenuCard> {
               value: asyncSnapshot.data ?? _enableTTS,
               onChanged: (bool value) {
                 setState(() => _enableTTS = value);
-                SharedPreferencesAsync().setBool(
+                unawaited(SharedPreferencesAsync().setBool(
                   TTSPreferencesKeys.htuTtsEnableTTS.name,
                   value,
-                );
+                ),);
               },
             ),
           ),
--- packages/flutter_heyteacher_text_to_speech/lib/src/tts/tts_view_model.dart
+++ packages/flutter_heyteacher_text_to_speech/lib/src/tts/tts_view_model.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:clock/clock.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_heyteacher_text_to_speech/src/tts/tts_data.dart';
 import 'package:flutter_heyteacher_utils/firebase.dart';
 import 'package:flutter_heyteacher_utils/locale.dart';
@@ -9,52 +10,74 @@ import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+/// A view model that manages Text-To-Speech (TTS) functionality.
+///
+/// This class is a singleton that handles initializing the TTS engine,
+/// setting the language based on the app's locale, and providing a method
+/// to speak text with throttling and duplicate-text prevention.
 class TTSViewModel {
+  TTSViewModel._() {
+    _textToSpeech = FlutterTts();
+    unawaited(_textToSpeech.awaitSpeakCompletion(false));
+    // get locale language
+    final languageCode = LocaleViewModel.instance.locale?.languageCode ??
+        Intl.getCurrentLocale();
+    unawaited(_changeLanguage(languageCode));
+    // listen locale languale change
+    _stateChangesStreamSubscription = LocaleViewModel.instance.localeStream
+        .listen((locale) => _changeLanguage(locale.languageCode));
+  }
   static final _logger = Logger('TTSViewModel');
 
   late FlutterTts _textToSpeech;
 
   String? _previousTextSpeaked;
 
-  StreamSubscription? _onUserUpdateStreamSubscription,
-      _stateChangesStreamSubscription;
+  StreamSubscription<Locale>?  _stateChangesStreamSubscription;
 
   static TTSViewModel? _instance;
 
   DateTime? _previousTextSpeakedDateTime;
 
+  /// The singleton instance of [TTSViewModel].
+  // ignore: prefer_constructors_over_static_methods
   static TTSViewModel get instance => _instance ??= TTSViewModel._();
-  TTSViewModel._() {
-    _textToSpeech = FlutterTts();
-    _textToSpeech.awaitSpeakCompletion(false);
-    // get locale language
-    final languageCode = LocaleViewModel.instance.locale?.languageCode ??
-        Intl.getCurrentLocale();
-    _changeLanguage(languageCode);
-    // listen locale languale change
-    _stateChangesStreamSubscription = LocaleViewModel.instance.localeStream
-        .listen((locale) => _changeLanguage(locale.languageCode));
-  }
 
+  /// Disposes of the resources used by the view model.
+  ///
+  /// This should be called when the view model is no longer needed to prevent
+  /// memory leaks.
   void dispose() {
-    _stateChangesStreamSubscription?.cancel();
-    _onUserUpdateStreamSubscription?.cancel();
+    unawaited(_stateChangesStreamSubscription?.cancel());
   }
 
+  /// Checks if Text-To-Speech is enabled.
+  ///
+  /// It first checks the user's preference in [SharedPreferencesAsync]. If not
+  /// set, it falls back to the value from [RemoteConfigViewModel].
   Future<bool> get enabled async =>
       (await SharedPreferencesAsync().getBool(
         TTSPreferencesKeys.htuTtsEnableTTS.name,
       )) ??
       RemoteConfigViewModel.instance.getBool('enableTTS');
 
+  /// Speaks the given [text] using the TTS engine.
+  ///
+  /// This method includes several checks:
+  /// - It will not speak if TTS is disabled.
+  /// - It will not speak if the [text] is the same as the previously spoken 
+  ///   text.
+  /// - If [checkTTSThreshold] is `true`, it will not speak if the time since
+  ///   the last speech is less than the threshold defined in remote config
+  ///   (`ttsThresholdInSeconds`).
   Future<void> speak(String text, {required bool checkTTSThreshold}) async {
-    _logger.finest('<speak>: text \'$text\'');
+    _logger.finest("<speak>: text '$text'");
     if (!await enabled) {
-      _logger.finest('(speak): TTS enabled, ignored text \'$text\'');
+      _logger.finest("(speak): TTS disabled, ignored text '$text'");
       return;
     }
     if (_previousTextSpeaked == text) {
-      _logger.finest('(speak): ignore text equals to previous text \'$text\'');
+      _logger.finest("(speak): ignore text equals to previous text '$text'");
       return;
     }
     final ttsThresholdInSeconds = RemoteConfigViewModel.instance
@@ -63,17 +86,17 @@ class TTSViewModel {
         _previousTextSpeakedDateTime != null &&
         clock.now().difference(_previousTextSpeakedDateTime!) <
             Duration(seconds: ttsThresholdInSeconds)) {
-      _logger.finest('(speak): ignore text \'$text\' too close to previous '
-          'speaked at \'$_previousTextSpeakedDateTime\' '
+      _logger.finest("(speak): ignore text '$text' too close to previous "
+          "speaked at '$_previousTextSpeakedDateTime' "
           'ttsThresholdInSeconds $ttsThresholdInSeconds');
       return;
     }
     _previousTextSpeaked = text;
     _previousTextSpeakedDateTime = clock.now();
-    _logger.finest('(speak): text \'$text\'');
-    _textToSpeech.speak(text);
+    _logger.finest("(speak): text '$text'");
+    unawaited(_textToSpeech.speak(text));
   }
 
-  void _changeLanguage(String? languageCode) async =>
-      await _textToSpeech.setLanguage(languageCode ?? Intl.getCurrentLocale());
+  Future<void> _changeLanguage(String? languageCode) async =>
+      _textToSpeech.setLanguage(languageCode ?? Intl.getCurrentLocale());
 }
