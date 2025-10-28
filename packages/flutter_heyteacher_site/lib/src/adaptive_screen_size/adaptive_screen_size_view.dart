5f83c2a8e75fa0a2da00d5653f9bb446136b4885
Hey, Teacher <heyteacher70@gmail.com>
2025-10-28 19:34:30 +0100

    screen renamed in _currentScreen and expoded via getter currentScreen

--- packages/flutter_heyteacher_site/lib/src/adaptive_screen_size/adaptive_screen_size_view.dart
+++ packages/flutter_heyteacher_site/lib/src/adaptive_screen_size/adaptive_screen_size_view.dart
@@ -3,17 +3,20 @@ import 'package:flutter_heyteacher_site/src/adaptive_screen_size/adaptive_screen
 
 /// A mixin to make a [State] object adaptive to screen size changes.
 ///
-/// It listens to media query changes and updates the [screen] property
+/// It listens to media query changes and updates the [_currentScreen] property
 /// accordingly.
 mixin AdaptiveScreenSizeState<T extends StatefulWidget> on State<T> {
   /// The current screen size category.
+  ScreenSize _currentScreen = ScreenSize.small;
+
   @protected
-  ScreenSize screen = ScreenSize.small;
+  /// The current screen size category.
+  ScreenSize get currentScreen => _currentScreen;
 
   @override
-  /// Updates the [screen] size when widget dependencies change.
+  /// Updates the [_currentScreen] size when widget dependencies change.
   void didChangeDependencies() {
     super.didChangeDependencies();
-    screen = ScreenSize.of(MediaQuery.of(context).size.width);
+    _currentScreen = ScreenSize.of(MediaQuery.of(context).size.width);
   }
 }
