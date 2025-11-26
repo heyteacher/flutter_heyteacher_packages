340ea86a927b2bf835fb8fd7501e0f2632978176
Hey, Teacher <heyteacher70@gmail.com>
2025-11-26 16:06:13 +0100

    remove logging in app check initialization

--- packages/flutter_heyteacher_utils/lib/src/firebase/app_check.dart
+++ packages/flutter_heyteacher_utils/lib/src/firebase/app_check.dart
@@ -1,7 +1,7 @@
 /// Configures and activates Firebase App Check for the application.
 ///
-/// Firebase App Check helps protect backend resources from abuse, such as 
-/// billing fraud or phishing, by ensuring that incoming requests originate 
+/// Firebase App Check helps protect backend resources from abuse, such as
+/// billing fraud or phishing, by ensuring that incoming requests originate
 /// from authentic app instances. This library sets up App Check with
 ///  appropriate providers
 /// for web, Android, and Apple platforms.
@@ -10,7 +10,6 @@ library;
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/foundation.dart';
-import 'package:logging/logging.dart';
 
 /// A view model for managing Firebase App Check initialization.
 ///
@@ -19,7 +18,6 @@ import 'package:logging/logging.dart';
 class AppCheckViewModel {
   /// Private constructor for the singleton pattern.
   AppCheckViewModel._();
-  final _logger = Logger('AppCheckModel');
 
   static AppCheckViewModel? _instance;
 
@@ -35,30 +33,20 @@ class AppCheckViewModel {
   /// - For Android, it uses `AndroidProvider.playIntegrity` in release mode
   ///   and `AndroidProvider.debug` in debug mode.
   /// - For Apple platforms (iOS/macOS), it uses `AppleProvider.appAttest`.
-  Future<void> initialize() async {
-    const androidAppCheckProvider = kDebugMode
-        ? AndroidDebugProvider()
-        : AndroidPlayIntegrityProvider();
-
-    // in debug mode, uncomment for test permission denied error in firesotre 
-    //forcing playIntegrity
-    //AndroidProvider androidProvider = AndroidProvider.playIntegrity;
-
-    _logger.info('androidAppCheckProvider $androidAppCheckProvider');
-
-    await FirebaseAppCheck.instance.activate(
-      // You can also use 6a `ReCaptchaEnterpriseProvider` provider instance as 
-      // an argument for `webProvider`
-      providerWeb: ReCaptchaEnterpriseProvider(
-        FirebaseRemoteConfig.instance.getString('appCheckReCaptchaV3SiteKey'),
-      ),
-      // Default provider for Android is the Play Integrity provider. You can 
-      // use the "AndroidProvider" enum to choose
-      // your preferred provider. Choose from:
-      // 1. Debug provider
-      // 2. Safety Net provider
-      // 3. Play Integrity provider
-      providerAndroid: androidAppCheckProvider,
-    );
-  }
+  Future<void> initialize() async => FirebaseAppCheck.instance.activate(
+    // You can also use 6a `ReCaptchaEnterpriseProvider` provider instance as
+    // an argument for `webProvider`
+    providerWeb: ReCaptchaEnterpriseProvider(
+      FirebaseRemoteConfig.instance.getString('appCheckReCaptchaV3SiteKey'),
+    ),
+    // Default provider for Android is the Play Integrity provider. You can
+    // use the "AndroidProvider" enum to choose
+    // your preferred provider. Choose from:
+    // 1. Debug provider
+    // 2. Safety Net provider
+    // 3. Play Integrity provider
+    providerAndroid: kDebugMode
+        ? const AndroidDebugProvider()
+        : const AndroidPlayIntegrityProvider(),
+  );
 }
