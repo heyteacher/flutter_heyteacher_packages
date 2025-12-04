ac72cabfe735226cf1b056e9ee453b2658f43bc6
Hey, Teacher <heyteacher70@gmail.com>
2025-12-04 17:41:29 +0100

    rename EnableTTSDropDownMenuCard in EnableTTSChoiceCard

--- packages/flutter_heyteacher_text_to_speech/lib/flutter_heyteacher_text_to_speech.dart
+++ packages/flutter_heyteacher_text_to_speech/lib/flutter_heyteacher_text_to_speech.dart
@@ -1,5 +1,5 @@
 export 'src/l10n/flutter_heyteacher_text_to_speech.dart'
     show FlutterHeyteacherTextToSpeechLocalizations;
 export 'src/tts/tts_view.dart'
-    show EnableTTSDropDownMenuCard;
+    show EnableTTSChoiceCard;
 export 'src/tts/tts_view_model.dart' show TTSViewModel;
