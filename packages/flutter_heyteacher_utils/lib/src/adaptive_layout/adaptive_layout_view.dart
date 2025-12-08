3e37abd0eb5cfdbc35970ae92d8177c955342677
Hey, Teacher <heyteacher70@gmail.com>
2025-12-08 14:42:10 +0100

    simplify adaptive layout implementation removing _LargeAdaptiveView , _MediumAdaptiveView and _SmallAdaptiveView repliced by _BranchView<PARAMS>

--- packages/flutter_heyteacher_utils/lib/src/adaptive_layout/adaptive_layout_view.dart
+++ packages/flutter_heyteacher_utils/lib/src/adaptive_layout/adaptive_layout_view.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_heyteacher_utils/src/adaptive_layout/adaptive_layout_data.dart'
     show ScreenSize;
 
-/// The state for the [_AbstractView].
+/// The state for the [_BranchView].
 ///
 /// 1° __Abstract__: this state implement the logic abstracting the screen size.
 ///
 /// Usually [build] returns a [GridView] or a [SliverGrid] settings
-/// crossAxisCount to [_AbstractView.crossAxisCount].
+/// crossAxisCount to [_BranchView.crossAxisCount].
 abstract class AbstractAdaptiveState<PARAMS>
-    extends State<_AbstractView<PARAMS>> {}
+    extends State<_BranchView<PARAMS>> {}
 
 /// The state for [SFW].
 ///
-/// 2° __Measures__ the current [ScreenSize] with [AdaptiveStateMixin] and
+/// 2° __Measures__ the current [ScreenSize] and
 ///   builds the corrisponding branch for [ScreenSize.small],
 /// [ScreenSize.medium] and [ScreenSize.large].
 abstract class AdaptiveState<
@@ -21,79 +21,7 @@ abstract class AdaptiveState<
   ST extends AbstractAdaptiveState<PARAMS>,
   PARAMS
 >
-    extends State<SFW>
-    with AdaptiveStateMixin<SFW> {
-  @override
-  Widget build(BuildContext context) => switch (currentScreenSize) {
-    ScreenSize.large => _LargeAdaptiveView(createAdaptiveState, params),
-    ScreenSize.medium => _MediumAdaptiveView(createAdaptiveState, params),
-    ScreenSize.small => _SmallAdaptiveView(createAdaptiveState, params),
-  };
-
-  /// The parameters object pass to abstract
-  PARAMS get params;
-
-  /// Returns an instance [ST] State.
-  ST createAdaptiveState();
-}
-
-/// The abstract statefull widget.
-///
-/// Exposes [crossAxisCount]
-abstract class _AbstractView<PARAMS> extends StatefulWidget {
-  /// Creates an instance of [_AbstractView].
-  @protected
-  const _AbstractView(this.createAdaptiveState, this.params);
-
-
-  /// the crossAxisCount which change on branch implementations
-  int get crossAxisCount;
-
-  final AbstractAdaptiveState<PARAMS> Function() createAdaptiveState;
-  
-  final PARAMS params;
-
-  @override
-  /// Creates the state for the [_AbstractView].
-  // ignore: no_logic_in_create_state
-  State<_AbstractView<PARAMS>> createState() => createAdaptiveState();
-}
-
-/// 3° __Branch__:  implements [_AbstractView] for small screens.
-class _LargeAdaptiveView<PARAMS> extends _AbstractView<PARAMS> {
-  const _LargeAdaptiveView(super.createAdaptiveState, super.params);
-
-  /// the crossAxisCount which change on branch implementations
-  @override
-  @protected
-  int get crossAxisCount => 3;
-}
-
-/// 3° __Branch__  implements [_AbstractView] for medium screens.
-class _MediumAdaptiveView<PARAMS> extends _AbstractView<PARAMS> {
-  const _MediumAdaptiveView(super.createAdaptiveState, super.params);
-
-  /// the crossAxisCount which change on branch implementations
-  @override
-  @protected
-  int get crossAxisCount => 2;
-}
-
-/// 3° __Branch__ implements [_AbstractView] for small screens.
-class _SmallAdaptiveView<PARAMS> extends _AbstractView<PARAMS> {
-  const _SmallAdaptiveView(super.createAdaptiveState, super.params);
-
-  /// the crossAxisCount which change on branch implementations
-  @override
-  @protected
-  int get crossAxisCount => 1;
-}
-
-/// A mixin to make a [State] object adaptive to screen size changes.
-///
-/// It listens to media query changes and updates the [_currentScreenSize]
-/// property accordingly.
-mixin AdaptiveStateMixin<T extends StatefulWidget> on State<T> {
+    extends State<SFW> {
   /// The current screen size category.
   ScreenSize _currentScreenSize = ScreenSize.small;
 
@@ -107,4 +35,60 @@ mixin AdaptiveStateMixin<T extends StatefulWidget> on State<T> {
     super.didChangeDependencies();
     _currentScreenSize = ScreenSize.of(MediaQuery.of(context).size.width);
   }
+
+  @override
+  Widget build(BuildContext context) => _BranchView(
+    currentScreenSize: currentScreenSize,
+    createAdaptiveState: createAdaptiveState,
+    params: params,
+  );
+
+  /// The parameters object pass to abstract
+  PARAMS get params;
+
+  /// Returns an instance [ST] State.
+  ST createAdaptiveState();
+}
+
+/// The branch statefull widget.
+///
+/// 3° __Branches__ implementation for [ScreenSize.small],
+/// [ScreenSize.medium] and [ScreenSize.large].
+
+/// Exposes:
+/// - [params]
+/// - [currentScreenSize]
+/// - [crossAxisCount] for [GridView] and [SliverGrid]
+class _BranchView<PARAMS> extends StatefulWidget {
+  
+  /// Creates an instance of [_BranchView].
+  @protected
+  const _BranchView({
+    required ScreenSize currentScreenSize,
+    required this.createAdaptiveState,
+    required PARAMS params,
+  }) : _params = params,
+       _currentScreenSize = currentScreenSize;
+
+  final ScreenSize _currentScreenSize;
+
+  final PARAMS _params;
+
+  ScreenSize get currentScreenSize => _currentScreenSize;
+
+  /// the crossAxisCount which change on branch implementations
+  int get crossAxisCount => switch (currentScreenSize) {
+    ScreenSize.large => 3,
+    ScreenSize.medium => 2,
+    ScreenSize.small => 1,
+  };
+
+  PARAMS get params => _params;
+
+  final AbstractAdaptiveState<PARAMS> Function() createAdaptiveState;
+
+  @override
+  /// Creates the state for the [_BranchView].
+  // ignore: no_logic_in_create_state
+  State<_BranchView<PARAMS>> createState() => createAdaptiveState();
 }
