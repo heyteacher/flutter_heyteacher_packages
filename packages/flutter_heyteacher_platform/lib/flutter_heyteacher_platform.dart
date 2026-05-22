083717a29ae030abde30084e8dd70dfeaf6f3feb
heyteacher <heyteacher70@gmail.com>
2026-05-22 12:29:31 +0200

    chore: reorganize `info_device_package` in separate folder and files

--- packages/flutter_heyteacher_platform/lib/flutter_heyteacher_platform.dart
+++ packages/flutter_heyteacher_platform/lib/flutter_heyteacher_platform.dart
@@ -1,6 +1,8 @@
 export 'src/context_helper.dart' show ContextHelper;
-export 'src/info_device_package.dart'
-    show DevicePackageInfoListTile, InfoDevicePackageViewModel;
+export 'src/info_device_package/info_device_package_view.dart'
+    show DevicePackageInfoListTile;
+export 'src/info_device_package/info_device_package_view_model.dart'
+    show InfoDevicePackageViewModel;
 export 'src/l10n/flutter_heyteacher_platform.dart'
     show FlutterHeyteacherPlatformLocalizations;
 export 'src/platform_helper.dart' show PlatformHelper;
--- packages/flutter_heyteacher_platform/lib/src/info_device_package/info_device_package_view_model.dart
+++ packages/flutter_heyteacher_platform/lib/src/info_device_package/info_device_package_view_model.dart
@@ -1,18 +1,4 @@
-/// Provides utilities for retrieving device and application package
-/// information and a widget to display this information along with a
-/// support request option.
-///
-/// This library includes:
-/// - [DevicePackageInfoListTile]: A [ListTile] widget that displays formatted
-///   device and package version information, and a button to initiate
-///   a support email.
-/// - [InfoDevicePackageViewModel]: A singleton class that fetches detailed
-///   device information (OS, model, browser) and package information
-///   (version, build number).
-library;
-
 import 'dart:async';
-
 import 'package:clock/clock.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
@@ -28,60 +14,6 @@ import 'package:flutter_heyteacher_views/flutter_heyteacher_views.dart';
 import 'package:logging/logging.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-/// A widget that displays device and package information in a list tile format.
-///
-/// It asynchronously fetches and shows the device identifier, device details
-/// (model, OS version),
-/// and the application's version and build number.
-/// It also includes a "Support" button that opens the default email client
-/// It shows the device type, version, and a button to ask for support.
-class DevicePackageInfoListTile extends StatelessWidget {
-  /// Creates a [DevicePackageInfoListTile].
-  const DevicePackageInfoListTile({required String supportEmail, super.key})
-    : _supportEmail = supportEmail;
-
-  final String _supportEmail;
-
-  @override
-  Widget build(BuildContext context) => ListTile(
-    key: const ValueKey('lt_fhu_version'),
-    leading: IconButton(
-      icon: const Icon(Icons.smartphone),
-      onPressed: () async {
-        InfoDevicePackageViewModel.instance._incrementTapCounter();
-        if (InfoDevicePackageViewModel.instance.tapCounterReached) {
-          showSnackBar(
-            context: context,
-            message: FlutterHeyteacherPlatformLocalizations.of(
-              context,
-            )!.advancedFeaturesUnlocked,
-          );
-        }
-      },
-    ),
-    title: FutureBuilder<String>(
-      future: InfoDevicePackageViewModel.instance.packageVersion,
-      builder: (_, devicePackageSnapshot) => Text(
-        devicePackageSnapshot.data ?? '',
-      ),
-    ),
-    subtitle: FutureBuilder(
-      future: InfoDevicePackageViewModel.instance.deviceInfo,
-      builder: (_, deviceSnapshot) => Text(
-        'id: ${deviceSnapshot.data}',
-      ),
-    ),
-    trailing: IconButton(
-      onPressed: () => InfoDevicePackageViewModel.instance._askSupport(
-        context: context,
-        supportEmail: _supportEmail,
-      ),
-      icon: const Icon(Icons.support),
-      tooltip: FlutterHeyteacherPlatformLocalizations.of(context)!.askSupport,
-    ),
-  );
-}
 
 /// A singleton model class responsible for fetching and providing
 /// device-specific information and application package details.
@@ -131,7 +63,7 @@ class InfoDevicePackageViewModel {
   bool get tapCounterReached => _tapCounterReached;
 
   /// Increments the tap counter and broadcasts the new value.
-  void _incrementTapCounter() {
+  void incrementTapCounter() {
     _tapCounterReached = (++_tapCounter) >= 5;
     _tapCounterReachedStreamController.sink.add(_tapCounterReached);
   }
@@ -229,7 +161,7 @@ class InfoDevicePackageViewModel {
   /// - A subject line indicating the app name.
   /// - A body containing the user's identifier, device information, and app
   ///   version, formatted for easy support.
-  Future<void> _askSupport({
+  Future<void> askSupport({
     required BuildContext context,
     required String supportEmail,
   }) async {
