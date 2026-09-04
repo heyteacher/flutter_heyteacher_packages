681375cd42c50e936deda147afe1eb93cbce5bd5
heyteacher <heyteacher70@gmail.com>
2026-09-04 07:40:08 +0200

    feat: replace `connectivity_plus` with `internet_connection_checker_plus` for network monitoring detecting network unreachable

--- packages/flutter_heyteacher_connectivity/lib/src/connectivity_view_model.dart
+++ packages/flutter_heyteacher_connectivity/lib/src/connectivity_view_model.dart
@@ -1,18 +1,15 @@
-
 import 'dart:async';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:logging/logging.dart';
-
 
 /// A singleton controller for monitoring network connectivity status.
 ///
-/// This class uses the `connectivity_plus` package to provide a stream
-/// of connectivity changes and a method to check the current connectivity 
-/// state.
+/// This class uses the `internet_connection_checker_plus` package to provide a
+/// stream of connectivity changes and a method to check the current
+/// connectivity state.
 class ConnectivityViewModel {
-
   /// Disposes the singleton instance of [ConnectivityViewModel)
 
   ConnectivityViewModel._() {
@@ -22,7 +19,8 @@ class ConnectivityViewModel {
   }
   static final _logger = Logger('ConnectivityViewModel');
 
-  final Connectivity _connectivity = Connectivity();
+  final InternetConnection _internetConnection =
+      InternetConnection.createInstance();
   StreamSubscription<bool>? _streamSubscription;
 
   static ConnectivityViewModel? _instance;
@@ -40,29 +38,24 @@ class ConnectivityViewModel {
   ///
   /// This should be called when the controller is no longer needed.
   void dispose() {
+    unawaited(_internetConnection.dispose());
     unawaited(_streamSubscription?.cancel());
   }
 
-  /// A stream that emits the connectivity status whenever it changes.
+  /// A stream that emits the internet status whenever it changes.
   ///
-  /// Emits `true` if there is at least one active connection 
-  /// (e.g., WiFi, mobile data).
-  /// Emits `false` if there are no active connections 
-  /// (`ConnectivityResult.none`).
-  Stream<bool> get stream => _connectivity.onConnectivityChanged.map(
-      (connectivityResultList) => connectivityResultList
-          .where((connectivityResult) =>
-              connectivityResult != ConnectivityResult.none)
-          .isNotEmpty);
+  /// Emits `true` if there is an active internet connection
+  /// Emits `false` if there is no active internet connection
+  Stream<bool> get stream => _internetConnection.onStatusChange.map(
+    (internetStatus) => internetStatus == InternetStatus.connected,
+  );
 
-  /// A future that completes with the current connectivity status.
+  /// A future that completes with the current internet status.
   ///
-  /// Returns `true` if there is at least one active connection, 
-  /// `false` otherwise.
-  Future<bool> get connected async => (await _connectivity.checkConnectivity())
-      .where(
-          (connectivityResult) => connectivityResult != ConnectivityResult.none)
-      .isNotEmpty;
+  /// Returns `true` if there is an active internet connection
+  /// Returns `false` if there is no active internet connection
+  Future<bool> get connected async =>
+      (await _internetConnection.internetStatus) == InternetStatus.connected;
 
   /// A future that completes with the opposite of [connected].
   Future<bool> get notConnected async => !await connected;
